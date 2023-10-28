9361cdaf8d302aceaf97988eaf80d05ca2cca981
mahalingam <mahalingam.vs@lntinfotech.com>
2023-10-29 02:01:22 +0530

    Vehicle tracking changes

--- src/components/Dashboard/VehicleTracking/VehicleTracking.js
+++ src/components/Dashboard/VehicleTracking/VehicleTracking.js
@@ -1,13 +1,54 @@
 import React, { useState } from 'react';
+
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
 import './VehicleTracking.css';
+import axios from 'axios';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
 function VehicleTracking({ inactive }) {
-  const [showSuccessPopup, SetShowSuccessPopup] = useState(true);
   const [vehicle, setVehicle] = useState('');
+  const [vehicleData, setVehicleData] = useState('');
+  const [open, setOpen] = useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (vehicle !== '') {
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+          setVehicleData(response.data);
+          handleClickOpen();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      console.log('hello');
+    }
   };
+
   return (
     <div className={`contact-body ${!inactive ? 'contact-inactive' : ''} `}>
       <div class="section-header">
@@ -46,6 +87,56 @@ function VehicleTracking({ inactive }) {
           </div>
         </div>
       </div>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Vehicle Details
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          <table>
+            <tr>
+              <th>
+                <Typography gutterBottom>Vehicle Number</Typography>
+              </th>
+              <td>
+                <Typography gutterBottom>
+                  {vehicleData?.vehicleNumber || ' '}
+                </Typography>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <Typography gutterBottom>Registered Address</Typography>
+              </th>
+              <td>
+                <Typography gutterBottom>
+                  {vehicleData?.addressLine1 || ' '}
+                </Typography>
+              </td>
+            </tr>
+          </table>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
     </div>
   );
 }
